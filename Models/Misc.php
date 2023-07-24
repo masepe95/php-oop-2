559eb2283d06bfa3f673a44e2ea1e3a07aaffdb8
masepe95 <marcosepe10@gmail.com>
2023-07-24 19:15:27 +0200

    misc model added

---
+++ Models/Misc.php
@@ -0,0 +1,28 @@
+<?php
+require_once __DIR__ . './products.php';
+require_once __DIR__ . './category.php';
+
+class Misc extends Product
+{
+    public $material;
+    public $size;
+
+    function __construct(
+        Category $_category,
+        string $_productName,
+        float $_price,
+        string $_imgPath,
+        string $_material,
+        string $_size
+    ) {
+        parent::__construct(
+            $_category,
+            $_productName,
+            $_price,
+            $_imgPath
+        );
+
+        $this->material = $_material;
+        $this->size = $_size;
+    }
+}

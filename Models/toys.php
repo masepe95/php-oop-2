29cc1cdab0bcff8f42a7ebdeb0e384490c0d568d
masepe95 <marcosepe10@gmail.com>
2023-07-24 19:07:08 +0200

    toy model added

---
+++ Models/toys.php
@@ -0,0 +1,28 @@
+<?php
+require_once __DIR__ . './products.php';
+require_once __DIR__ . './category.php';
+
+class Toy extends Product
+{
+    public $features;
+    public $size;
+
+    function __construct(
+        Category $_category,
+        string $_productName,
+        float $_price,
+        string $_imgPath,
+        string $_features,
+        string $_size
+    ) {
+        parent::__construct(
+            $_category,
+            $_productName,
+            $_price,
+            $_imgPath
+        );
+
+        $this->features = $_features;
+        $this->size = $_size;
+    }
+}
